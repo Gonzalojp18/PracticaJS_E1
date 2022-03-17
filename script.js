6bacf512284be8bb5114793e7df24ac8004cf23c
Gonzalojp18 <pgonzalojose@gmail.com>
2022-03-17 13:54:40 -0300

    print date type using var

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ console.log(18)
 
 
 alert ("Hola Mundo, Me llamo Gonzalo Palomo ")
+
+// variables
+
+var cantidadDeVentanasEnMiCasa  = 6
+console.log( "En mi casa hay: " + cantidadDeVentanasEnMiCasa + " " + "ventanas")
+
+var soyHumano = true
+console.log(soyHumano)
+
+var miGustoDePizzaPreferido = ("Marinera")
+console.log(miGustoDePizzaPreferido)
+
+var changeName = false
+console.log(changeName)
+
+changeName = 'hola'
+console.log(changeName)
+
+changeName = 0
+console.log(changeName)
+
+changeName = 'Minions'
+console.log(changeName)
+
+
+
+
+
+
+
