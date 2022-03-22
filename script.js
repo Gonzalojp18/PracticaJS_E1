4cd535c4554f0e097b312f35add6aeb4d1288c5c
Gonzalojp18 <pgonzalojose@gmail.com>
2022-03-21 22:30:18 -0300

    exercise of variable and logical E 3.2

--- script.js
+++ script.js
@@ -32,6 +32,93 @@ console.log(changeName)
 changeName = 'Minions'
 console.log(changeName)
 
+// EJERCICIOS VARIABLES N° 3-1
+
+var noValgoNi5 = 4
+console.log(noValgoNi5)
+
+var miAnioDeNacimiento = 1989
+console.log(miAnioDeNacimiento)
+
+var  meGustariaTener80Anios = 80
+console.log(meGustariaTener80Anios)
+
+var voyATener80ElAnio = 1989+80
+console.log(voyATener80ElAnio)
+
+var Tendre80Anio = 2069
+console.log("Tendre la edad de 80 years en el:" +" "+ Tendre80Anio)
+
+var costoDeUnaTele = 10000
+console.log(costoDeUnaTele)
+
+var ahorro = 9000
+console.log(ahorro)
+
+var dineroQueMeFalta = 10000-9000
+console.log(dineroQueMeFalta)
+
+var  diasPorAnio = 365
+console.log( diasPorAnio)
+
+var cantidadDeAniosEnUnaDecada = 10
+console.log(cantidadDeAniosEnUnaDecada)
+
+var cantidadDeDiasEnUnaDecada = 365*10
+console.log(cantidadDeDiasEnUnaDecada)
+
+
+var golesEquipo1 = 14;
+
+var golesEquipo2 = 10;
+
+var ganoEquipo1 = golesEquipo1 > golesEquipo2;
+
+var empate = golesEquipo1 === golesEquipo2;
+
+console.log(ganoEquipo1); // true
+
+console.log(empate); // false
+
+var miEdad = 32
+
+var edadMedia = 17
+
+var soyMayorDeEdad = (32>17)
+
+console.log(soyMayorDeEdad)
+
+var tuEdad = 32
+
+var otraEdad = 25
+
+var SoyMenorDe25 = (tuEdad<otraEdad)
+
+console.log(SoyMenorDe25)
+
+
+var mesActual = 30
+var mes = "enero"
+
+noEstamosEnEnero = 30==="enero"
+console.log(noEstamosEnEnero)
+
+var miNotaEnElParcial = 8
+
+var notaMinimaParaAprobar = 6
+
+var estoyAprobado = (miNotaEnElParcial >= notaMinimaParaAprobar)
+
+console.log("Actualmente te encuentras aprobado: " + estoyAprobado)
+
+var horaActual = 10
+
+var horaAlmuerzo = 12
+
+
+var esHoraDeAlmorzar = (horaActual <= horaAlmuerzo)
+
+console.log(esHoraDeAlmorzar)
 
 
 
